487734acf744ab640c399a0a59f0bd984d458141
ɽÖñ <2231256557@qq.com>
2023-02-14 17:31:37 +0800

    更新接口文档

--- news-commons/news-common-base/src/main/java/com/common/base/result/R.java
+++ news-commons/news-common-base/src/main/java/com/common/base/result/R.java
@@ -1,6 +1,5 @@
 package com.common.base.result;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.HashMap;
@@ -10,16 +9,12 @@ import java.util.Map;
 @Data
 public class R {
 
-    @ApiModelProperty(value = "是否成功")
     private Boolean success;
 
-    @ApiModelProperty(value = "返回码")
     private Integer code;
 
-    @ApiModelProperty(value = "返回消息")
     private String message;
 
-    @ApiModelProperty(value = "返回数据")
     private Map<String, Object> data = new HashMap<String, Object>();
 
     //把构造方法私有
--- news-commons/news-common-swagger/src/main/java/com/common/swagger/config/SwaggerConfig.java
+++ news-commons/news-common-swagger/src/main/java/com/common/swagger/config/SwaggerConfig.java
@@ -6,53 +6,193 @@ package com.common.swagger.config;
  * @description: TODO
  * @date 2023/2/8 9:36
  */
-
+import cn.hutool.core.lang.Console;
+import com.common.base.constant.ApplicationConst;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
-@EnableSwagger2
 @Configuration
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
-    @Bean(value = "defaultApi2")
-    public Docket defaultApi2() {
-        ApiSelectorBuilder builder = new Docket(DocumentationType.SWAGGER_2)
-                .enableUrlTemplating(false)
-                .apiInfo(apiInfo())
-                // 选择那些路径和api会生成document
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    private static final String URL = "http://www.ycframework.com/";
+
+    private static final String AUTHOR_EMAIL = "youcongtech@163.com";
+
+    private static final String VERSION = "1.0";
+
+    private static final String GROUP_NAME = "1.0版本";
+
+    @Bean(value = "api")
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
                 .select()
-                // 对所有api进行监控
                 .apis(RequestHandlerSelectors.any())
-                //这里可以自定义过滤
-                .paths(this::filterPath);
-
-        return builder.build();
-    }
-
-    private boolean filterPath(String path) {
-        boolean ret = path.endsWith("/error");
-        if (ret) {
-            return false;
-        }
-        //这块可以写其他的过滤逻辑
-        return true;
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("title")
-                .description("description")
-                .termsOfServiceUrl("https://www.baidu.com")
-                .version("1.0")
-                .contact(new Contact("nitric oxide", "www.baidu.com", "123@qq.com"))
+                .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo groupApiInfo(){
+        ApiInfo build = new ApiInfoBuilder()
+                .title(getTitle(applicationName))
+                .description(getDesc(applicationName))
+                .termsOfServiceUrl(URL)
+                .contact(AUTHOR_EMAIL)
+                .version(VERSION)
+                .build();
+        return build;
+    }
+
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title(getTitle(applicationName))
+                        .description(getDesc(applicationName))
+                        .termsOfServiceUrl(URL)
+                        .contact(AUTHOR_EMAIL)
+                        .version(VERSION)
+                        .build())
+                //分组名称
+                .groupName(GROUP_NAME)
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage(getBasePackage(applicationName)))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    /**
+     * 包扫描自定义
+     *
+     * @param applicationName
+     * @return
+     */
+    private String getBasePackage(String applicationName) {
+        Console.log("applicationName:" + applicationName);
+        String basePackage = "";
+        switch (applicationName) {
+            case ApplicationConst.USER:
+                basePackage = ApplicationConst.KNIFE4J_USER_PACKAGE;
+                break;
+//            case ApplicationConst.ADMIN:
+//                basePackage = ApplicationConst.KNIFE4J_ADMIN_PACKAGE;
+//                break;
+//            case ApplicationConst.CMS:
+//                basePackage = ApplicationConst.KNIFE4J_CMS_PACKAGE;
+//                break;
+//            case ApplicationConst.CRAWLER:
+//                basePackage = ApplicationConst.KNIFE4J_CRAWLER_PACKAGE;
+//                break;
+//            case ApplicationConst.FILE:
+//                basePackage = ApplicationConst.KNIFE4J_FILE_PACKAGE;
+//                break;
+//            case ApplicationConst.PLUGINS:
+//                basePackage = ApplicationConst.KNIFE4J_PLUGINS_PACKAGE;
+//                break;
+//            case ApplicationConst.WECHAT:
+//                basePackage = ApplicationConst.KNIFE4J_WECHAT_PACKAGE;
+//                break;
+//            default:
+//                basePackage = ApplicationConst.DEFAULT_PACKAGE;
+//                break;
+        }
+        Console.log("basePackage:" + basePackage);
+        return basePackage;
+    }
+
+    /**
+     * 服务名称
+     *
+     * @param applicationName
+     * @return
+     */
+    private String getTitle(String applicationName) {
+        Console.log("applicationName:" + applicationName);
+        String title = "";
+        switch (applicationName) {
+            case ApplicationConst.USER:
+                title = "用户管理服务中心";
+                break;
+//            case ApplicationConst.ADMIN:
+//                title = "统一用户管理";
+//                break;
+//            case ApplicationConst.CMS:
+//                title = "统一内容管理";
+//                break;
+//            case ApplicationConst.CRAWLER:
+//                title = "统一爬虫管理";
+//                break;
+//            case ApplicationConst.FILE:
+//                title = "统一文件管理";
+//                break;
+//            case ApplicationConst.PLUGINS:
+//                title = "统一插件管理";
+//                break;
+//            case ApplicationConst.WECHAT:
+//                title = "微信生态管理";
+//                break;
+//            default:
+//                title = "默认应用管理";
+//                break;
+        }
+        Console.log("title:" + title);
+        return title;
+    }
+
+    /**
+     * 描述
+     *
+     * @param applicationName
+     * @return
+     */
+    private String getDesc(String applicationName) {
+        Console.log("applicationName:" + applicationName);
+        String desc = "";
+        switch (applicationName) {
+            case ApplicationConst.USER:
+                desc = "USER API";
+                break;
+//            case ApplicationConst.ADMIN:
+//                desc = "User API";
+//                break;
+//            case ApplicationConst.CMS:
+//                desc = "CMS API";
+//                break;
+//            case ApplicationConst.CRAWLER:
+//                desc = "Crawler API";
+//                break;
+//            case ApplicationConst.FILE:
+//                desc = "File API";
+//                break;
+//            case ApplicationConst.PLUGINS:
+//                desc = "Plugins API";
+//                break;
+//            case ApplicationConst.WECHAT:
+//                desc = "Wechat API";
+//                break;
+//            default:
+//                desc = "Default API";
+//                break;
+        }
+        Console.log("desc:" + desc);
+        return desc;
+    }
 }
+
